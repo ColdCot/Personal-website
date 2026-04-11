76225abed25b094a4aeaa3d2c8a718ad1810b17d
ColdCot <160887893+ColdCot@users.noreply.github.com>
2026-04-11 16:11:32 +0200

    Wrote unit tests for the SenderService (#16)
    * Implemented unit tests for the SenderService
    
    *Removed unused Unit test placeholder

---
+++ XUnitTest/SenderServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Personal_website.DB;
+using Personal_website.Models;
+using Personal_website.Services;
+
+namespace XUnitTest;
+
+public class SenderServiceTest
+{ 
+    private static (WebsiteDbContext context, SenderService service) CreateTestEnvironment()
+    {
+        var options = new DbContextOptionsBuilder<WebsiteDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+    
+        var context = new WebsiteDbContext(options);
+        var service = new SenderService(context);
+    
+        return (context, service);
+    }
+    [Fact]
+    public async Task ReturnsEmptyIfTableIsEmpty()
+    {
+        var (context, service) = CreateTestEnvironment();
+        
+        await using (context)
+        {
+            var result = await service.GetAllAsync();
+            result.Should().BeEmpty();
+        }
+    }
+
+    [Fact]
+    public async Task ReturnsSendersWhenNotEmpty()
+    {
+        var (context, service) = CreateTestEnvironment();
+
+        await using (context)
+        {
+            var data = new List<Sender>
+            {
+                new Sender { Id = 1, Name = "Test1", Email = "test1@gmail.com" },
+                new Sender { Id = 2, Name = "Test2", Email = "test2@gmail.com" },
+                new Sender { Id = 3, Name = "Test3", Email = "test3@gmail.com" },
+                new Sender { Id = 4, Name = "Test4", Email = "test4@gmail.com" },
+                new Sender { Id = 5, Name = "Test5", Email = "test5@gmail.com" },
+                new Sender { Id = 6, Name = "Test6", Email = "test6@gmail.com" },
+            };
+
+            context.Senders.AddRange(data);
+
+            await context.SaveChangesAsync();
+
+            var result = await service.GetAllAsync();
+
+            result.Should().BeEquivalentTo(data);
+        }
+    }
+}
